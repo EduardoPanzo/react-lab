2ba0e01cf17adaa88500262e73295bca6ccf2acd
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-04-04 14:03:54 +0100

    feat: sidebar

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,48 @@
-export function Lesson() {
-  return <div>Lesson</div>;
+import { CheckCircle, Lock } from "phosphor-react";
+import { isPast, format } from "date-fns";
+import pt from "date-fns/locale/pt";
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: "live" | "class";
+}
+
+export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const isLessonAvailable = isPast(availableAt);
+  const avaliableDateFormatted = format(
+    availableAt,
+    "EEEE' • 'd' de 'MMMM' • 'K'h'mm",
+    {
+      locale: pt,
+    }
+  );
+  return (
+    <a href={`/event/lesson/${slug}`} className="group">
+      <span className="text-gray-300">{avaliableDateFormatted}</span>
+
+      <div className="rounded border border-gray-500 p-4 pt-2">
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
+              Conteúdo Liberado
+            </span>
+          ) : (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+
+          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
+            {type === "live" ? " AO VIVO" : "AULA PRÁTICA"}
+          </span>
+        </header>
+
+        <strong className="text-gray-200 mt-5 block">{title}</strong>
+      </div>
+    </a>
+  );
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -4,10 +4,12 @@ import { Video } from "../components/Video";
 
 export function Event() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <Sidebar />
-      <Video />
+      <main className="flex flex-1">
+        <Video />
+        <Sidebar />
+      </main>
     </div>
   );
 }
