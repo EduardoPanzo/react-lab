d23af56edf0a70634a924193bdacbdca728bb96f
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-04-07 07:13:19 +0100

    feat: video component structure

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,103 @@
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Image,
+  Lightning,
+} from "phosphor-react";
+
 export function Video() {
-  return <div className="flex-1">Video</div>;
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          video
+        </div>
+      </div>
+
+      <div className="p8 max-w-[1100px] mx-auto">
+        <div className="flex itens-start gap-16">
+          <div className="flex-1">
+            <h1 className="txt-2xl font-bold">
+              Aula 01 - Abertura do Ignate çab
+            </h1>
+
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+              itaque excepturi tenetur at dolorum, sed perspiciatis odit cum
+              facilis sit.
+            </p>
+
+            <div className="flex itens-center gap-4 mt-6">
+              <img src="https://github.com/eduardoPanzo.png" alt="" />
+
+              <div>
+                <strong className="font-bold text-2xl block">
+                  João E. Panzo
+                </strong>
+                <span className="text-gray-200 text-sm block">Front-End</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <a
+              href="#"
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2  justify-center hover:bg-green-700 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Comunidade do discord
+            </a>
+            <a
+              href="#"
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2  justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
+              <Lightning size={24} />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a
+            href="#"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Material complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu
+                desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight />
+            </div>
+          </a>
+
+          <a
+            href="#"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <Image size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers exclusivos</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
+                máquina
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
